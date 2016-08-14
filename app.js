7772d7f06f3fe0a94ba5ff2167996f4eaa213038
aghh1504 <agatta1504@interia.pl>
2016-08-14 11:57:32 +0000

    want to join both array together

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require("express");
 var request = require("request");
 var app = express();
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + "/views"))
+app.use(express.static(__dirname + "/views"));
+
+
 
 app.get("/", function(req,res){
    res.render("searchtfl") 
@@ -20,15 +22,27 @@ app.get("/tfl", function(req,res){
     });
 });
 // looking for lng adress
-app.get("/map", function(req,res){
-    var url ="https://maps.googleapis.com/maps/api/geocode/json?address="+"Annesley Avenue"+"London+UK&AIzaSyAy7pE9UvY-M1mENT3ER49LyOjztju6wIs";
+// app.get("/map", function(req,res){
+//     var url ="https://maps.googleapis.com/maps/api/geocode/json?address="+"Annesley Avenue"+"London+UK&AIzaSyAy7pE9UvY-M1mENT3ER49LyOjztju6wIs";
+//         request(url, function(error,response,body){
+//             if(!error && response.statusCode == 200){
+//               var data = JSON.parse(body);
+//                 res.render("map", {data: data});
+//             } 
+//         });
+//     });
+
+
+ app.get("/map", function(req,res){
+    var url ="https://maps.googleapis.com/maps/api/geocode/json?address="+ /*[array of busstop station]*/ +"London+UK&AIzaSyAy7pE9UvY-M1mENT3ER49LyOjztju6wIs";
         request(url, function(error,response,body){
             if(!error && response.statusCode == 200){
               var data = JSON.parse(body);
-                res.render("map", {data: data});
+                res.render("markers", {data: data});
             } 
         });
     });
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("movie server started");
