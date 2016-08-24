e6cd540dc8687f7bb04be2765bdaca0f4d6d3584
aghh1504 <agatta1504@interia.pl>
2016-08-24 18:36:55 +0000

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var request = require("request");
 var app = express();
+var stop;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/views"));
 
@@ -19,6 +20,9 @@ app.get("/tfl", function(req,res){
                 var data = JSON.parse(body);
                     res.render("tfl", {data: data});
          } 
+          stop = data[0]["commonName"];
+               console.log(data[0]["lat"], data[0]["lon"]);
+        
     });
 });
 // looking for lng adress
@@ -34,11 +38,12 @@ app.get("/tfl", function(req,res){
 
 
  app.get("/map", function(req,res){
-    var url ="https://maps.googleapis.com/maps/api/geocode/json?address="+ /*[array of busstop station]*/ +"London+UK&AIzaSyAy7pE9UvY-M1mENT3ER49LyOjztju6wIs";
+      var query = req.query.stop;
+      var url ="https://maps.googleapis.com/maps/api/geocode/json?address="+ query/*[array of busstop station]*/ +"London+UK&AIzaSyAy7pE9UvY-M1mENT3ER49LyOjztju6wIs";
         request(url, function(error,response,body){
             if(!error && response.statusCode == 200){
               var data = JSON.parse(body);
-                res.render("markers", {data: data});
+                res.render("map", {data: data});
             } 
         });
     });
